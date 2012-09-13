8bc9d6417d120d1409fa2d375c255ab61e82d514
Jacob Vesterlund <jacobvesterlund@gmail.com>
2012-09-13 22:10:35 +0200

    Clean-up and change of package names and artifact id..

---
+++ src/test/java/se/elva/test/testng/softassert/SoftTestBaseTest.java
@@ -0,0 +1,19 @@
+package se.elva.test.testng.softassert;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners({ SoftTestListener.class })
+public class SoftTestBaseTest extends SoftTestBase {
+
+	@Test
+	public void softAssertWillPassTest() {
+		softAssertTrue(true);
+	}
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void softAssertWillFailTest() {
+        assertTrue(false);
+    }
+	
+}
